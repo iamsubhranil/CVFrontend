4cbdd7f7c85b9f42ba2672926f03292c60f5ed5e
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2023-11-06 01:57:27 +0530

    new: cv versions and professional font Now, the version of cv to view can be specified using ver= parameter. ver can currently be "cloud" or "cpp", and defaults to cpp at this time. All of the project information is now consolidated in projects.js, and is automatically populated in the page based on ver. Also, in print view, now the page uses Computer Modern, the latex font.

--- src/count.js
+++ src/count.js
@@ -1,3 +1,5 @@
+import { data } from "./projects.js";
+
 function hit() {
   // Set the ID of the HTML element that will display the visitor count
   var visitorCountElement = document.getElementById("visitor-count");
@@ -14,14 +16,79 @@ function hit() {
   xhr.send();
 }
 
+function designBody() {
+	var version = "cpp";
+
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if(urlParams.has("ver")) {
+		version = urlParams.get("ver");
+	}
+
+	var content = data[0];
+
+	data.forEach(element => {
+		if(element.version == version) {
+			content = element;
+		}
+	});
+
+	var summary = document.getElementById("summary");
+	summary.innerHTML = content.summary;
+
+	var showcaseList = document.getElementById("showcase");
+	content.showcase.forEach(element => {
+		var name = document.createElement("h3");
+		name.className = "fit-item";
+		name.innerHTML = "<b>" + element.name + "</b>";
+		var points = document.createElement("ul");
+		element.details.forEach(point => {
+			var p = document.createElement("li");
+			p.innerHTML = point;
+			points.appendChild(p);
+		});
+
+		var show = document.createElement("li");
+		show.appendChild(name);
+		if (element.link != null) {
+			var link = document.createElement("h5");
+			link.className = "fit-item";
+			link.innerHTML = "<a href=" + element.link + ">" + element.link + "</a>";
+			show.appendChild(link);
+		}
+		show.appendChild(points);
+
+		showcaseList.appendChild(show);
+	});
+
+	var othersList = document.getElementById("others");
+	content.others.forEach(other => {
+		var o = document.createElement("li");
+		o.innerHTML = "<a href=" + other.link + "><b>" + other.name + "</b></a> - " + other.desc;
+		othersList.appendChild(o);
+	});
+}
+
+function changeFontForAll(cls) {
+	var h1s = document.querySelectorAll(cls);
+	h1s.forEach(element => {
+		element.style.fontFamily = "Computer Modern Bold";
+	});
+}
+
 function checkAndReformat() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const printable = urlParams.has("printable");
+  const printable = urlParams.has("print");
   if (printable) {
+	changeFontForAll("h1");
+	changeFontForAll("h2");
+	changeFontForAll("b");
+
     document.getElementById("body").style.lineHeight = 1.2;
     document.getElementById("body").style.backgroundColor = "white";
     document.getElementById("body").style.color = "black";
+	document.getElementById("body").style.fontFamily = 'Computer Modern';
     document.getElementById("main").style.maxWidth = "100%";
     document.getElementById("main").style.margin = "0";
     document.getElementById("heading").style.display = "grid";
@@ -30,7 +97,6 @@ function checkAndReformat() {
     document.getElementById("heading").style.justifyContent = "left";
     document.getElementById("heading").style.paddingBottom = "0.5rem";
     document.getElementById("one-liner").style.display = "none";
-    document.getElementById("fullname").style.fontFamily = "Open Sans";
     const links = document.getElementById("links");
     var emailId = null;
     for (let i = 0; i < links.children.length; i++) {
@@ -46,7 +112,7 @@ function checkAndReformat() {
     document.getElementById("location").innerHTML += " | " + emailId;
     if (urlParams.has("phone")) {
       document.getElementById("location").innerHTML +=
-        " | " + urlParams.get("phone");
+        " | +91" + urlParams.get("phone");
     }
 
     const skillSection = document.getElementById("skillsection");
@@ -69,6 +135,7 @@ function checkAndReformat() {
 
 function prepare() {
   hit();
+  designBody();
   checkAndReformat();
 }
 
